9943efdd247653392bd56615b98a63f36d880924
Arriverdecci <yuri.cardosovasconcelos@gmail.comm>
2023-06-30 15:57:23 -0300

    version "0.0.2"

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,5 +3,9 @@ from routers import user, book
 
 app = FastAPI()
 
+@app.get("/")
+async def home():
+    return {"version": "0.0.2"}
+
 app.include_router(user.router)
 app.include_router(book.router)
