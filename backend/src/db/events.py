0181539548cd517065bd336df18d6608c954b11e
Jonathan Bispo <jonathanbispo.pessoal@gmail.com>
2023-07-12 10:07:54 -0300

    fix startup issues

--- backend/src/db/events.py
+++ backend/src/db/events.py
@@ -1,3 +1,4 @@
+import asyncio
 import asyncpg
 from fastapi import FastAPI
 from loguru import logger
@@ -8,44 +9,105 @@ from core.settings.app import AppSettings
 async def connect_to_db(app: FastAPI, settings: AppSettings) -> None:
     logger.info("Connecting to PostgreSQL")
 
-    app.state.pool = await asyncpg.create_pool(
-        str(settings.database_url),
-        min_size=settings.min_connection_count,
-        max_size=settings.max_connection_count,
-    )
+    for _ in range(settings.db_conn_max_retry_count):
+        try:
+            app.state.pool = await asyncpg.create_pool(
+            str(settings.database_url),
+            min_size=settings.min_connection_count,
+            max_size=settings.max_connection_count,
+            )
+            break
+        except ConnectionRefusedError:
+            await asyncio.sleep(settings.db_conn_retry_interval)
 
     logger.info("Connection established")
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL,bio TEXT DEFAULT '', image TEXT, salt TEXT NOT NULL,hashed_password TEXT NOT NULL,created_at TIMESTAMP WITHOUT TIME ZONE,updated_at TIMESTAMP WITHOUT TIME ZONE);"
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS users (\n"
+        "    id SERIAL PRIMARY KEY,\n"
+        "    username TEXT NOT NULL,\n"
+        "    email TEXT NOT NULL,\n"
+        "    bio TEXT DEFAULT '',\n"
+        "    image TEXT,\n"
+        "    salt TEXT NOT NULL,\n"
+        "    hashed_password TEXT NOT NULL,\n"
+        "    created_at TIMESTAMP WITHOUT TIME ZONE,\n"
+        "    updated_at TIMESTAMP WITHOUT TIME ZONE\n"
+        ");"
     )
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS public.books ( id SERIAL PRIMARY KEY, slug TEXT UNIQUE NOT NULL, title TEXT NOT NULL, description TEXT NOT NULL, body TEXT NOT NULL, author_id INTEGER, created_at TIMESTAMP, updated_at TIMESTAMP, ratings INTEGER[], average_rating DOUBLE PRECISION, CONSTRAINT books_author_id_fkey FOREIGN KEY (author_id) REFERENCES public.users (id) ON DELETE SET NULL);"
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS public.books (\n"
+        "    id SERIAL PRIMARY KEY,\n"
+        "    slug TEXT UNIQUE NOT NULL,\n"
+        "    title TEXT NOT NULL,\n"
+        "    description TEXT NOT NULL,\n"
+        "    body TEXT NOT NULL,\n"
+        "    author_id INTEGER,\n"
+        "    created_at TIMESTAMP,\n"
+        "    updated_at TIMESTAMP,\n"
+        "    ratings INTEGER[],\n"
+        "    average_rating DOUBLE PRECISION,\n"
+        "    CONSTRAINT books_author_id_fkey FOREIGN KEY (author_id) REFERENCES public.users (id) ON DELETE\n"
+        "    SET\n"
+        "        NULL\n"
+        ");"
     )
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS tags ( tag TEXT PRIMARY KEY );"   
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS tags (\n"
+        "    tag TEXT PRIMARY KEY\n"
+        ");"   
     )
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS books_to_tags (book_id INT, tag TEXT, FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE, FOREIGN KEY (tag) REFERENCES tags(tag) ON DELETE CASCADE, PRIMARY KEY (book_id, tag));"
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS books_to_tags (\n"
+        "    book_id INT,\n"
+        "    tag TEXT,\n"
+        "    FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,\n"
+        "    FOREIGN KEY (tag) REFERENCES tags(tag) ON DELETE CASCADE,\n"
+        "    PRIMARY KEY (book_id, tag)\n"
+        ");"
     )
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS favorites (user_id INT, book_id INT, FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE, PRIMARY KEY (user_id, book_id));"
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS favorites (\n"
+        "    user_id INT,\n"
+        "    book_id INT,\n"
+        "    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,\n"
+        "    FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,\n"
+        "    PRIMARY KEY (user_id, book_id)\n"
+        ");"
     )
 
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS reviews(id INT PRIMARY KEY, commentarie TEXT NOT NULL, rating INT, author_id INT, book_id INT, FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE, created_at TIMESTAMP, updated_at TIMESTAMP);" 
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS reviews(\n"
+        "    id INT PRIMARY KEY,\n"
+        "    commentarie TEXT NOT NULL,\n"
+        "    rating INT, author_id INT,\n"
+        "    book_id INT,\n"
+        "    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,\n"
+        "    FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,\n"
+        "    created_at TIMESTAMP,\n"
+        "    updated_at TIMESTAMP\n"
+        ");" 
     )
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS profile (id SERIAL PRIMARY KEY, username TEXT NOT NULL, bio TEXT, image TEXT, created_at TIMESTAMP WITHOUT TIME ZONE, updated_at TIMESTAMP WITHOUT TIME ZONE);"
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS profile (\n"
+        "    id SERIAL PRIMARY KEY,\n"
+        "    username TEXT NOT NULL,\n"
+        "    bio TEXT, image TEXT,\n"
+        "    created_at TIMESTAMP WITHOUT TIME ZONE,\n"
+        "    updated_at TIMESTAMP WITHOUT TIME ZONE\n"
+        ");"
     )
     
-    app.state.pool.execute(
-        "CREATE TABLE IF NOT EXISTS followers_to_followings (follower_id INTEGER, following_id INTEGER);"
+    await app.state.pool.execute(
+        "CREATE TABLE IF NOT EXISTS followers_to_followings (\n"
+        "    follower_id INTEGER,\n"
+        "    following_id INTEGER\n"
+        ");"
     )
 
 async def close_db_connection(app: FastAPI) -> None:
