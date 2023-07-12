4ad4f5a0960625b2bdde45df101acf22701457cd
Arrivedercci <yuri.cardosovasconcelos@gmail.com>
2023-07-12 14:10:05 -0300

    review fix

--- backend/src/db/events.py
+++ backend/src/db/events.py
@@ -83,7 +83,7 @@ async def connect_to_db(app: FastAPI, settings: AppSettings) -> None:
     await app.state.pool.execute(
         "CREATE TABLE IF NOT EXISTS reviews(\n"
         "    id INT PRIMARY KEY,\n"
-        "    commentarie TEXT NOT NULL,\n"
+        "    comment TEXT NOT NULL,\n"
         "    rating INT, author_id INT,\n"
         "    book_id INT,\n"
         "    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,\n"
